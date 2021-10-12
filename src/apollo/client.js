4766a88a5cf74ad9c8c8d9fedb1f3131beb2b4ac
AhmedMalick <ahmedmalik2k20@gmail.com>
2021-10-12 12:34:13 +0500

    updated

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,10 +1,12 @@
 import fetch from "cross-fetch";
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-export const client = new ApolloClient({
+const client = new ApolloClient({
   link: new HttpLink({
     uri: "/.netlify/functions/graphql_faunadb",
     fetch,
   }),
   cache: new InMemoryCache(),
 });
+
+export default client
